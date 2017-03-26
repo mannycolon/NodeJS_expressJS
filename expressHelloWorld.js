e807c409eaf8f7a23938c1914fb22bb0d256f342
mannycolon <colonmanuel7@gmail.com>
2017-03-26 15:50:02 -0400

    added optional portNumber coming from process argument in node. changed the directory from '/' to '/home'

--- expressHelloWorld.js
+++ expressHelloWorld.js
@@ -1,7 +1,7 @@
  var express = require('express')
  var app = express()
  
-app.get('/', function(req, res) {
+app.get('/home', function(req, res) {
   res.end('Hello World!')
   next()
 })
@@ -11,3 +11,4 @@ app.get('*', function(req, res) {
 })
 
 app.listen(3000)
+// app.listen(process.argv[2])
