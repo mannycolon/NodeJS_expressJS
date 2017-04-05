4987c6b16b412c0133ae1f6236f8d7f811a40284
mannycolon <colonmanuel7@gmail.com>
2017-04-04 21:08:35 -0400

    express pugs

--- expressPug.js
+++ expressPug.js
@@ -26,7 +26,7 @@
 var path = require('path')
 var express = require('express')
 var app = express()
-    app.set('view engine', 'jade')
+app.set('view engine', 'jade')
 app.set('views', process.argv[3])
 app.get('/home', function(req, res) {
     res.render('index', {date: new Date().toDateString()})
